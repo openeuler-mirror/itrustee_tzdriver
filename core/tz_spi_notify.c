3f816c33b2ed4fa5dec60d467108763cbff5d599
袁浩 <yuanhao34@huawei.com>
2023-09-27 17:28:40 +0800

    fit for linux 6.4
    Signed-off-by: 袁浩 <yuanhao34@huawei.com>

--- core/tz_spi_notify.c
+++ core/tz_spi_notify.c
@@ -443,8 +443,7 @@ static void spi_broadcast_notifications(void)
 		return;
 	}
 
-	missed = (uint32_t)__xchg(0, &g_notify_data->meta.context.meta.missed,
-		MISSED_COUNT);
+	missed = (uint32_t)xchg(&g_notify_data->meta.context.meta.missed, 0);
 	if (missed == 0)
 		return;
 	if ((missed & (1U << NOTIFY_DATA_ENTRY_WAKEUP)) != 0) {
